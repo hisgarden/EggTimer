e8bb93919de8e0791efe5b3e833e539616c786a1
Jin Wen <jin.wen@hisgarden.org>
2023-11-02 01:06:24 -0700

    Refactor again to print out the dictionary string usng unwrapped

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -18,17 +18,19 @@ class ViewController: UIViewController {
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
         print(sender.currentTitle ?? "default value")
-        let hardness = sender.currentTitle
-        switch hardness {
-            case "Soft":
-                print(eggTimes["Soft"]!)
-            case "Medium":
-                print(eggTimes["Medium"]!)
-            case "Hard":
-                print(eggTimes["Hard"]!)
-            default:
-                print("Error")
-        }
+        let hardness = sender.currentTitle! //Soft, Medium, Hard
+        //        switch hardness {
+        //            case "Soft":
+        //                print(eggTimes["Soft"]!)
+        //            case "Medium":
+        //                print(eggTimes["Medium"]!)
+        //            case "Hard":
+        //                print(eggTimes["Hard"]!)
+        //            default:
+        //                print("Error")
+        //        }
+        print(eggTimes[hardness]!)
+        
     }
     
 }
